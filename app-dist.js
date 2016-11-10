0b6d89943a6f158884070f32bb41e9368d2ab64a
eriklevoll <erik.levoll@gmail.com>
2016-11-10 16:51:55 +0200

    Removed blur

--- app-dist.js
+++
@@ -1,1 +0,0 @@
-!function(a){var n=a(".download-paragraph-wrapper");n.on("click",function(){window.location="TLKit Manager.rar"})}(jQuery);
--- app.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-  var mainButton = $('.download-paragraph-wrapper');
-
-  mainButton.on('click', function() {
-     window.location = "TLKit Manager.rar";
-  });
-})(jQuery);
